14f553061a43ec5e4273a1429f090aadc2787b01
luke <luke.ng@sandboxvr.com>
2023-08-24 12:06:23 +0800

    install vimplug

--- nvim.bat
+++ nvim.bat
@@ -1,1 +1,2 @@
+curl.exe -fLo %LOCALAPPDATA%/nvim-data/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 mklink /j %homedrive%%homepath%\AppData\Local\nvim %~dp0\nvim
