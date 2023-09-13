d4cebcfceb3f953ff6c4c25cb3fa1e2bf6bf1300
luke <luke.ng@sandboxvr.com>
2023-09-13 19:12:50 +0800

    fix nvim.sh

--- nvim.sh
+++ nvim.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/
 ln -s "$(readlink -f $(dirname "$0"))/nvim" ~/.config/nvim
