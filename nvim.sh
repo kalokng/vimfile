baa1a9de7480fe7ac5545d399be393147f437a87
kalokng <kalokng@gmail.com>
2025-11-20 23:29:03 +0800

    fix

--- nvim.sh
+++ nvim.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir -p ~/.config/
-ln -s "$(readlink -f $(dirname "$0"))/nvim" ~/.config/nvim
+if [ -n "$XDG_CONFIG_HOME" ]; then
+    CONFIG_DIR="$XDG_CONFIG_HOME"
+else
+    CONFIG_DIR=~
+    mkdir -p ~/.config/
+fi
+ln -s "$(readlink -f $(dirname "$0"))/nvim" "$CONFIG_DIR/nvim"
