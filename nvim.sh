4f56b474a9378c2167683fc2e95f3e8822a7f5c4
luke <luke.ng@sandboxvr.com>
2026-04-16 18:33:19 +0800

    update

--- nvim.sh
+++ nvim.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 if [ -n "$XDG_CONFIG_HOME" ]; then
     CONFIG_DIR="$XDG_CONFIG_HOME"
 else
